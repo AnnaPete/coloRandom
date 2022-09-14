d0cf3273be7498fc4a634bdfa29ad631731947c8
Tim T <tim.email.personal@gmail.com>
2022-09-13 19:38:29 -0700

    Add color class

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-// help
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+let hexValues = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let hexArray = [];
+
+class Color {
+    constructor() {
+        this.locked = false;
+        this.hexCode = this.createHexCode();
+    }
+    createHexCode() {
+        for (var i = 0; i < 6; i++) {
+            var randomIndex = getRandomIndex(hexValues);
+            var randomValue = hexValues[randomIndex];
+            hexArray.push(randomValue);
+            var commaSeparated = hexArray.join("")
+            commaSeparated = `#${commaSeparated}`
+        }
+        return commaSeparated
+    }
+}
+
+var newColor = new Color()
